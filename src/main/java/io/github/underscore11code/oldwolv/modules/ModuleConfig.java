6877337dd4170e99a2ee91659dcd200a9d4f19f8
underscore11code <minecrafter11mrt@gmail.com>
2020-01-16 13:00:43 -0800

    add "addstaff", "remstaff", and "staff"

--- src/main/java/io/github/underscore11code/oldwolv/modules/ModuleConfig.java
+++ src/main/java/io/github/underscore11code/oldwolv/modules/ModuleConfig.java
@@ -3,7 +3,10 @@ package io.github.underscore11code.oldwolv.modules;
 import io.github.underscore11code.oldwolv.OldWolv;
 import io.github.underscore11code.oldwolv.config.GuildConfig;
 import io.github.underscore11code.oldwolv.util.CommandUtil;
+import io.github.underscore11code.oldwolv.util.PermissionUtil;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.permission.PermissionType;
+import org.javacord.api.entity.permission.Role;
 
 import java.awt.*;
 import java.lang.reflect.Method;
@@ -67,6 +70,87 @@ public class ModuleConfig {
             EmbedBuilder embed = CommandUtil.getTemplateEmbed();
             embed.setTitle("Disabled commands in " + cmd.getServer().get().getName());
             embed.setDescription(out);
+            embed.setColor(Color.BLUE);
+            CommandUtil.sendReply(cmd.getRawEvent(), embed);
+        } else
+            CommandUtil.sendUserError(cmd.getRawEvent(), "Not in a server!", "");
+    }
+
+    @Command(triggers = "addstaff", args = "<command>", helpMsg = "Adds a role as staff")
+    public static void commandAddStaff(CommandInfo cmd) {
+        if (cmd.getServer().isPresent()) {
+            if (cmd.isServerAdmin()) {
+                if (cmd.getArgs().size() != 1)
+                    CommandUtil.sendUserError(cmd.getRawEvent(), "Expected 1 argument, received " + cmd.getArgs().size(), "Stynax: `addstaff <command>`");
+                else {
+                    String id = cmd.getArgs().get(0).replace("<@&", "").replace(">", "");
+                    if (cmd.getServer().get().getRoleById(id).isPresent()) {
+                        GuildConfig config = GuildConfig.get(cmd.getServer().get().getIdAsString());
+                        ArrayList<String> staffRoles = config.getStaffRoleIds();
+                        staffRoles.add(id);
+                        config.setStaffRoleIds(staffRoles);
+                        config.save();
+                        CommandUtil.sendReply(cmd.getRawEvent(), CommandUtil.getTemplateEmbed().setColor(Color.GREEN).setTitle("Added staff role " + cmd.getArgs().get(0)));
+                    } else
+                        CommandUtil.sendUserError(cmd.getRawEvent(), "Unknown Role!", "");
+                }
+            } else
+                CommandUtil.sendUserError(cmd.getRawEvent(), "You don't have permission!", "");
+        } else
+            CommandUtil.sendUserError(cmd.getRawEvent(), "Not in a server!", "");
+    }
+
+    @Command(triggers = {"remstaff", "removestaff"}, args = "<command>", helpMsg = "Adds a role as staff")
+    public static void commandRemStaff(CommandInfo cmd) {
+        if (cmd.getServer().isPresent()) {
+            if (cmd.isServerAdmin()) {
+                if (cmd.getArgs().size() != 1)
+                    CommandUtil.sendUserError(cmd.getRawEvent(), "Expected 1 argument, received " + cmd.getArgs().size(), "Stynax: `addstaff <command>`");
+                else {
+                    String id = cmd.getArgs().get(0).replace("<@&", "").replace(">", "");
+                    GuildConfig config = GuildConfig.get(cmd.getServer().get().getIdAsString());
+                    if (config.getStaffRoleIds().contains(id)) {
+                        ArrayList<String> staffRoles = config.getStaffRoleIds();
+                        staffRoles.remove(id);
+                        config.setStaffRoleIds(staffRoles);
+                        config.save();
+                        CommandUtil.sendReply(cmd.getRawEvent(), CommandUtil.getTemplateEmbed().setColor(Color.GREEN).setTitle("Added staff role " + cmd.getArgs().get(0)));
+                    } else
+                        CommandUtil.sendUserError(cmd.getRawEvent(), "Unknown Role!", "");
+                }
+            } else
+                CommandUtil.sendUserError(cmd.getRawEvent(), "You don't have permission!", "");
+        } else
+            CommandUtil.sendUserError(cmd.getRawEvent(), "Not in a server!", "");
+    }
+
+    @Command(triggers = "staff", args = "", helpMsg = "Lists all staff roles")
+    public static void commandStaffRoles(CommandInfo cmd) {
+        if (cmd.getServer().isPresent()) {
+            String manuallyAdded = "";
+            for (String staffRole : GuildConfig.get(cmd.getServer().get().getIdAsString()).getStaffRoleIds()) {
+                manuallyAdded = manuallyAdded + "<@&" + staffRole + ">\n";
+            }
+            String roles = "";
+            for (Role role : cmd.getServer().get().getRoles()) {
+                boolean isStaff = false;
+                for (PermissionType allowedPermission : role.getAllowedPermissions()) {
+                    for (PermissionType staffPerm : PermissionUtil.getStaffPerms()) {
+                        if (allowedPermission.equals(staffPerm)) {
+                            isStaff = true;
+                            break;
+                        }
+                    }
+                }
+                if (isStaff)
+                    roles = roles + role.getMentionTag() + "\n";
+            }
+            EmbedBuilder embed = CommandUtil.getTemplateEmbed();
+            embed.setTitle("Staff roles in " + cmd.getServer().get().getName());
+            System.out.println("Characters in desc: " + roles.length());
+            embed.setDescription(roles);
+            if (manuallyAdded != "")
+                embed.addField("Manually added", manuallyAdded);
             embed.setColor(Color.BLUE);
             CommandUtil.sendReply(cmd.getRawEvent(), embed);
         } else
