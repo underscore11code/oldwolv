f87257551c3eb18d455a98808ef46a2982520e81
underscore11code <minecrafter11mrt@gmail.com>
2020-01-16 13:21:39 -0800

    added info command

--- src/main/java/io/github/underscore11code/oldwolv/modules/ModuleInfo.java
+++ src/main/java/io/github/underscore11code/oldwolv/modules/ModuleInfo.java
@@ -3,6 +3,7 @@ package io.github.underscore11code.oldwolv.modules;
 import io.github.underscore11code.oldwolv.OldWolv;
 import io.github.underscore11code.oldwolv.util.CommandUtil;
 import io.github.underscore11code.oldwolv.util.PrettyUtil;
+import io.github.underscore11code.oldwolv.util.VersionUtil;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
@@ -24,5 +25,18 @@ public class ModuleInfo {
     @Command(triggers = {"ping", "p"}, args = "", helpMsg = "Sees how quickly the bot can respond")
     public static void commandPing(CommandInfo cmd) {
         CommandUtil.sendReply(cmd.getRawEvent(), CommandUtil.getTemplateEmbed().setTitle("Pong!"));
+    }
+
+    @Command(triggers = "info", args = "", helpMsg = "Prints info about the bot")
+    public static void commandInfo(CommandInfo cmd) {
+        EmbedBuilder embed = CommandUtil.getTemplateEmbed();
+        embed.setTitle(OldWolv.getApi().getYourself().getName());
+        embed.setDescription("A discord bot made by Underscore11");
+        embed.setThumbnail(OldWolv.getApi().getYourself().getAvatar());
+        embed.addField("Github", "[underscore11code/oldwolv](https://github.com/underscore11code/oldwolv)");
+        embed.addField("Libraries used", "[Javacord](https://javacord.org/) - Discord API\n[Gson](https://github.com/google/gson) - Configuration saving and loading");
+        embed.addField("Version", OldWolv.getVersion() + "\nCommit " + VersionUtil.get("git.commit.id") + "\nBranch " + VersionUtil.get("git.branch"));
+        embed.setColor(Color.BLUE);
+        CommandUtil.sendReply(cmd.getRawEvent(), embed);
     }
 }
