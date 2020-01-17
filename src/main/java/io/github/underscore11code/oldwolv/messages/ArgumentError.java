95442b76b920e18627ff4a952f983250b1d198b0
underscore11code <minecrafter11mrt@gmail.com>
2020-01-16 22:07:44 -0800

    args are usually arraylists

--- src/main/java/io/github/underscore11code/oldwolv/messages/ArgumentError.java
+++ src/main/java/io/github/underscore11code/oldwolv/messages/ArgumentError.java
@@ -3,6 +3,8 @@ package io.github.underscore11code.oldwolv.messages;
 import io.github.underscore11code.oldwolv.util.PrettyUtil;
 import lombok.Getter;
 
+import java.util.ArrayList;
+
 public class ArgumentError implements Message {
     @Getter MessageType messageType = MessageType.ERROR;
     @Getter String title = "Argument Error";
@@ -10,5 +12,9 @@ public class ArgumentError implements Message {
 
     public ArgumentError(String[] expectedArgs, String[] receivedArgs) {
         this.description = String.format(description, expectedArgs.length, PrettyUtil.prettyArray(expectedArgs), receivedArgs.length, receivedArgs == null ? "Null" : PrettyUtil.prettyArray(receivedArgs));
+    }
+
+    public ArgumentError(String[] expectedArgs, ArrayList<String > receivedArgs) {
+        this.description = String.format(description, expectedArgs.length, PrettyUtil.prettyArray(expectedArgs), receivedArgs.size(), receivedArgs == null ? "Null" : PrettyUtil.prettyArrayList(receivedArgs));
     }
 }
