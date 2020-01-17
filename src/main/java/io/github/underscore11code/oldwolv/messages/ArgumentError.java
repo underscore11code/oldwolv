2ab3c95fd0822a99d0b0f3141a8e5e7f3e3dfedb
underscore11code <minecrafter11mrt@gmail.com>
2020-01-16 22:03:42 -0800

    added ArgumentError

---
+++ src/main/java/io/github/underscore11code/oldwolv/messages/ArgumentError.java
@@ -0,0 +1,14 @@
+package io.github.underscore11code.oldwolv.messages;
+
+import io.github.underscore11code.oldwolv.util.PrettyUtil;
+import lombok.Getter;
+
+public class ArgumentError implements Message {
+    @Getter MessageType messageType = MessageType.ERROR;
+    @Getter String title = "Argument Error";
+    @Getter String description = "Expected %s args (%s), received %s (%s)";
+
+    public ArgumentError(String[] expectedArgs, String[] receivedArgs) {
+        this.description = String.format(description, expectedArgs.length, PrettyUtil.prettyArray(expectedArgs), receivedArgs.length, receivedArgs == null ? "Null" : PrettyUtil.prettyArray(receivedArgs));
+    }
+}
