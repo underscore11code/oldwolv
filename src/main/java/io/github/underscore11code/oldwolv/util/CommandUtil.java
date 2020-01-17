6d47844ca4882582be67da7945b6f21aea7a47c6
underscore11code <minecrafter11mrt@gmail.com>
2020-01-16 21:57:18 -0800

    now can send Messages
    Also regex ID cleaner

--- src/main/java/io/github/underscore11code/oldwolv/util/CommandUtil.java
+++ src/main/java/io/github/underscore11code/oldwolv/util/CommandUtil.java
@@ -8,6 +8,7 @@ import org.javacord.api.event.message.MessageCreateEvent;
 
 import java.awt.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 
 public class CommandUtil {
     public static CompletableFuture<Message> sendReply(MessageCreateEvent event, EmbedBuilder builder, String reaction) {
@@ -19,11 +20,19 @@ public class CommandUtil {
         return sendReply(event, builder, "\u2705");
     }
 
+    public static CompletableFuture<Message> sendReply(MessageCreateEvent event, io.github.underscore11code.oldwolv.messages.Message message) {
+        return sendReply(event, getTemplateEmbed().setColor(message.getMessageType().getEmbedColor()).setTitle(message.getTitle()).setDescription(message.getDescription()), "\u2705");
+    }
+
     public static EmbedBuilder getTemplateEmbed() {
         return new EmbedBuilder().setTimestampToNow().setFooter(OldWolv.getApi().getYourself().getName() + " v" + OldWolv.getVersion());
     }
 
     public static void sendUserError(MessageCreateEvent event, String title, String desc) {
         sendReply(event, getTemplateEmbed().setTitle(title).setDescription(desc).setColor(Color.RED), "\u274C");
+    }
+
+    public static String cleanId(String id) {
+        return Pattern.compile("<[@#][!&]?([0-9]+)>").matcher(id.trim()).group(1);
     }
 }
